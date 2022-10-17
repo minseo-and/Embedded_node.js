416fd0549585d7f53e8969a00c61201e609dc7e4
minseo-and <msp05070478@gmail.com>
2022-10-17 20:13:39 +0900

    mongoDB

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const express = require("express");
-const server = express();
+// const express = require("express");
+// const server = express();
+//
+// server.use(express.static(__dirname + "/public"));
+//
+// server.get("/", (req, res) => {
+//
+//     res.sendFile(__dirname + "/index.html");
+// });
+//
+// server.get("/about", (req, res) => {
+//     res.sendFile(__dirname + "/about.html");
+// });
+//
+// server.listen(3000, (err) => {
+//     if (err) return console.log(err);
+//     console.log("The server is listening on port 3000");
+// });
 
-server.use(express.static(__dirname + "/public"));
+const mongoose = require("mongoose");
+const MONGODB_URL =
+    "mongodb+srv://test:test1234@education.wnf4ybm.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(
+MONGODB_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connect to database successfully");
+        }
+    }
+);
 
-server.get("/", (req, res) => {
-
-    res.sendFile(__dirname + "/index.html");
-});
-
-server.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/about.html");
-});
-
-
-server.listen(3000, (err) => {
-    if (err) return console.log(err);
-    console.log("The server is listening on port 3000");
-});
